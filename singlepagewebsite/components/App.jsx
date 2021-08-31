782fb893d976f0acf94858abc757440213f4487a
soumyamahesh2 <88882177+soumyamahesh2@users.noreply.github.com>
2021-08-31 05:46:38 +0530

    Add files via upload

--- singlepagewebsite/components/App.jsx
+++ singlepagewebsite/components/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Card from "./Card";
 import Campaign from "./Campaign";
-alert("hi");
+
 function CreatCard(campaignList) {
     return (
       <Card
-        key={campaignList.campaign_name}
+        key={campaignList.id}
+        campaign_name={campaignList.campaign_name}
         status={campaignList.status}
         created_at={campaignList.created_at}
         method={campaignList.method}
@@ -21,7 +22,10 @@ function App() {
         <span>Campaigns</span>
       </h1>
       <table>
-        {Campaign.map(CreatCard)}
+      <tr>
+      {Campaign.map(CreatCard)}
+      </tr>
+       
       </table>
     </div>
     );
