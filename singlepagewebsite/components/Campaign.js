63b23f2be393b0b3bb5c9f1eb2307cd0b693ff64
soumyamahesh2 <88882177+soumyamahesh2@users.noreply.github.com>
2021-08-31 05:47:41 +0530

    Add files via upload

--- singlepagewebsite/components/Campaign.js
+++ singlepagewebsite/components/Campaign.js
@@ -1,26 +1,26 @@
 const Campaign=[
-    {
+    {   id:1,
         campaign_name:"Website Promotion- Landing",
         status: "success",
         Created_at:"17 jun 2021",
         method: "CSV",
         media: "video"
     },
-    {
+    {   id:2,
         campaign_name:"App Download",
         status: "Failed",
         Created_at:"15 jun 2021",
         method: "URL Override",
         media: "video"
     },
-    {
+    {   id:3,
         campaign_name:"Social Media promotion",
         status: "success",
         Created_at:"14 jun 2021",
         method: "CSV",
         media: "video"
     },
-    {
+    {   id:4,
         campaign_name:"Linkedin Templates",
         status: "success",
         created_at:"13 jun 2021",
